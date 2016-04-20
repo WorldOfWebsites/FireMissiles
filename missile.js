f40ea8dacc7bf1a19606829b162afacdaf1e0cee
sweeper3000 <sweeper3000@gmail.com>
2016-04-19 21:08:50 -0400

    Update2
    Created card with more details.

--- missile.js
+++ missile.js
@@ -6,6 +6,19 @@ function FireMissiles() {
 	else {
 		var confirmMissiles = "You want missiles sent to";
 		confirm(confirmMissiles + " " + name + "?");
+		
+		var code1 = "Contacting computer... SUCCESS";
+		document.getElementById("code1").innerHTML = code1;
+		
+		var code2 = "Getting location... SUCCESS";
+		document.getElementById("code2").innerHTML = code2;
+		
+		var code3 = "Missiles launching in 3..2..1";
+		document.getElementById("code3").innerHTML = code3;
+		
+		var code4 = "Missiles launched";
+		document.getElementById("code4").innerHTML = code4
+		
 		var sentanceSuffix = "Missiles sucessfully sent to";
 		document.getElementById("result").innerHTML = sentanceSuffix + " " + name + ".";
 	}
