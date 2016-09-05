2a1362f9f79cd99e22034ad4bd68acaabfdf3b21
sweeper3000 <sweeper3000@users.noreply.github.com>
2016-09-05 00:26:44 +0000

    Deploy native code to production (#1)
    * Add java version
    
    * Add info for java
    
    * Add python2 and 3 files, edit readme and rm semicolon
    
    * Add ruby file and readme info; edit sentence in readme

---
+++ python/python3/FireMissiles.py
@@ -0,0 +1,14 @@
+person = input("Who should the missiles be sent to? ")
+print("You want missiles sent to " + person + "? Y/N")
+confirm = input();
+
+if confirm == "Y" or confirm == "y":
+	print("Contacting computer... SUCCESS")
+	print("Getting location... SUCCESS")
+	print("Missiles launching in 3..2..1")
+	print("Missiles launched")
+	print("Missiles sucessfully sent to " + person + ".")
+elif confirm == "N" or confirm == "n":
+	print("Operation aborted.")
+else:
+	print("Invalid response.")
