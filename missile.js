6d91c5e19a3f6271b312dfb38c3a2e2b13727cc8
sweeper3000 <spiketwofish@protonmail.com>
2016-09-05 22:41:01 -0400

    Rewrite is a work in progress...

--- missile.js
+++ missile.js
@@ -1,25 +1,33 @@
 function FireMissiles() {
-	var name = prompt("Where should the missiles be sent to?\nDISCLAIMER: This will not actually fire missiles");
-	if (name == null) {
-		alert("Please enter location.");
+	var location = prompt("Where should the missiles be sent to?\nDISCLAIMER: This will not actually fire missiles");
+
+	if (location == null) {
+		alert("Please enter a locaion");
 	}
-	else {
-		var confirmMissiles = "You want missiles sent to";
-		confirm(confirmMissiles + " " + name + "?");
-		
-		var code1 = "Contacting computer... SUCCESS";
-		document.getElementById("code1").innerHTML = code1;
-		
-		var code2 = "Getting location... SUCCESS";
-		document.getElementById("code2").innerHTML = code2;
-		
-		var code3 = "Missiles launching in 3..2..1";
-		document.getElementById("code3").innerHTML = code3;
-		
-		var code4 = "Missiles launched";
-		document.getElementById("code4").innerHTML = code4
-		
-		var sentanceSuffix = "Missiles sucessfully sent to";
-		document.getElementById("result").innerHTML = sentanceSuffix + " " + name + ".";
-	}
+	else if {
+		var confirm = confirm("Send missiles to " + location);
+
+		if (confirm == true) {
+			var confirmMissiles = "You want missiles sent to";
+	                confirm(confirmMissiles + " " + name + "?");
+
+        	        var code1 = "Contacting computer... SUCCESS";
+        	        document.getElementById("code1").innerHTML = code1;
+
+         	        var code2 = "Getting location... SUCCESS";
+               	        document.getElementById("code2").innerHTML = code2;
+
+               	        var code3 = "Missiles launching in 3..2..1";
+	                document.getElementById("code3").innerHTML = code3;
+
+	                var code4 = "Missiles launched";
+	                document.getElementById("code4").innerHTML = code4
+
+	                var sentanceSuffix = "Missiles sucessfully sent to";
+	                document.getElementById("result").innerHTML = sentanceSuffix + " " + location + ".";
+
+		}
+		else {
+			alert("Operation aborted.");
+		}
 }
