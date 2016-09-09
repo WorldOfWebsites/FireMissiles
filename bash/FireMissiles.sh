4dec69c66d21c5bdd804ae2c78093b75fa02e17c
sweeper3000 <spiketwofish@protonmail.com>
2016-09-08 22:37:36 -0400

    Add bash script and readme section

---
+++ bash/FireMissiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -p "Where should the missiles be sent to?\nDISCLAIMER: This will not actually fire missiles " location
+read -p "Send missiles to $location? Y/N " confirm
+
+if [ $confirm = "Y" ]; then
+	echo "Contacting computer... SUCCESS"
+	echo "Getting location... SUCCESS"
+	echo "Missiles launching in 3..2..1"
+	echo "Missiles launched"
+	echo "Missiles sucessfully sent to $location"
+elif [ $confirm = "N" ]; then
+	echo "Operation aborted"
+else
+	echo "Invalid response"
+fi
+exit
+
+
